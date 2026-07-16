7b52ffb0311f7f1c79a03bc282765a54a2967b1f
agenciacarbonu <agenciacarbonu@gmail.com>
2026-07-15 23:22:43 -0300

    atualizacao este 01

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'v1_cache';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './paginas/inicio.html',
+  './paginas/produtos.html',
+  './paginas/contato.html',
+  './paginas/sobre.html'
+];
+
+// Instala o service worker e guarda os arquivos no cache do celular
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Responde usando o cache quando estiver sem internet
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
